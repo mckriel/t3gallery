2781319c77e2e9c0c0113887131d688e3fb4ec40
mckriel <mckriel@gmail.com>
2024-12-12 12:00:29 +0200

    image upload part 1

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
+import "~/styles/globals.css";
+import "@uploadthing/react/styles.css";
+
 import { ClerkProvider } from '@clerk/nextjs';
 import { TopNav } from "./_components/topnav";
-import "~/styles/globals.css";
 
 import { GeistSans } from "geist/font/sans";
 import { type Metadata } from "next";
